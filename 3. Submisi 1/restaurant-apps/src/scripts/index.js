7d8cb39001a5e00aee26523c274ef1d1d08e18b9
adityanegara <aditnegarabisnis@gmail.com>
2021-07-07 11:22:27 +0700

    Learn JS Clean Code

---
+++ 3. Submisi 1/restaurant-apps/src/scripts/index.js
@@ -0,0 +1,6 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/styles.scss';
+import {navSlide} from './animation/navbar'
+
+navSlide();
+console.log('Hello Coders! :)');
